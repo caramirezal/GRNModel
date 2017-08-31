5096557c99eb145c1a671874c6609217f862ba42
caramirezal <cramireza@ciencias.unam.mx>
2017-08-30 19:33:52 -0500

    fixing X1 X2 melt names bug

--- plotAttractors.a.R
+++ plotAttractors.a.R
@@ -12,15 +12,16 @@ plotAttractors.a<-function(data,rowOrder=FALSE,rowIndexes,
     data.u<-data.u[rowIndexes,]
   }
   data.m<-melt(data.u)
+  colnames(data.m) <- c("X1","X2","value")
   data.m$X2<-factor(data.m$X2,
                                 levels = unique(data.m$X2),
                                 ordered = TRUE)
   data.m$X1<-factor(data.m$X1,
                                 levels = unique(data.m$X1),
                                 ordered = TRUE)
-  gg<-ggplot(data = data.m, aes(x=X1, y=X2, colours=value,fill=value )) 
-  gg<-gg + geom_tile(color="white",size=0.1)  
-  gg<-gg + scale_fill_gradient(low = "black", high = "gray", limit = c(0,1))
+  gg<-ggplot(data = data.m, aes(x=X1, y=X2, colours=value,fill=value ))
+  gg<-gg + geom_tile(color="white",size=0.1)
+  gg<-gg + scale_fill_gradient(low = "red", high = "green", limit = c(0,1))
   gg<-gg + theme(text=element_text(size = 16),axis.text.x=element_text(angle = 90,vjust = 0.4,colour = "black",face = "bold",size = 15))
   gg<-gg + labs(x="",y="")
   gg<-gg + coord_equal()
